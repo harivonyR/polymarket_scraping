e2e19525f58cdec751e13d232fe7e250b6915f28
harivonyR* <harivonyratefiarison@gmail.com>
2026-01-31 20:29:24 +0300

    ADD title, volume parsing

--- script/polymarket.py
+++ script/polymarket.py
@@ -5,25 +5,47 @@ from utils.selector import find_with_all_classes, find_with_classes
 from bs4 import BeautifulSoup
 
 
-def get_cards(url):
-    response = website_crawler(url)
-    
-    soup = BeautifulSoup(response)
+def get_cards(response_soup):
+
     classes = ["transition", "justify-between", "rounded-md", "shadow-md"]
     cards = find_with_all_classes(soup,classes)
     
     return cards
 
-def get_detail(card):
-    # get title
-    data = {}
+def get_title(card):
+    """
+    input : 
+        card : a beautifull soup object of a polymarket card on item list
+    
+    output :
+        title : a string representing the title of the card
+    """
     title_classes = ["text-sm", "font-semibold", "w-fit", "line-clamp-3", "text-pretty"]
     title = find_with_classes(card,title_classes)
     
-    data["title"] = title.text
+    return title.text
     
-    return data
+
+def get_volume(card):
+    """
+    input : 
+        card : a beautifull soup object of a polymarket card on item list
     
+    output :
+        title : a string representing the volume placed in an item (can be a text e.g. : new)
+    """
+    # get the card footer
+    card_footer_classes = ["flex", "gap-2", "justify-between", "items-center", "w-full", "overflow-x-auto", "whitespace-nowrap"]
+    card_footer = find_with_classes(card, card_footer_classes)
+    
+    
+    # get volume
+    volume_classes = ["flex", "items-center", "gap-1"]
+    volume = find_with_classes(card_footer, volume_classes)
+    
+    return volume.text
+    
+
 
 if __name__ == "__main__" :
     culture_url = "https://polymarket.com/pop-culture"
@@ -32,18 +54,15 @@ if __name__ == "__main__" :
     #response = website_rendering(culture_url, wait_in_seconds=5, scroll=3)    # forbiden
     
     soup = BeautifulSoup(response)
-    classes = ["transition", "justify-between", "rounded-md", "shadow-md"]
-    cards = find_with_all_classes(soup,classes )
+    cards = get_cards(soup)
     
     res = []
     
     for card in cards :
         data = {}
-        # get title
-        title_classes = ["text-sm", "font-semibold", "w-fit", "line-clamp-3", "text-pretty"]
-        title = find_with_classes(card,title_classes)
         
-        data["title"] = title.text
+        data["title"] = get_title(card)
+        data["volume"] = get_volume(card)
         
         res.append(data)
         
